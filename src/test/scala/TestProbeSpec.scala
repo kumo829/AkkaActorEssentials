71349d371715ed60869cafd6582f762830016418
kumo829 <amonmar2000@hotmail.com>
2022-07-27 22:34:58 -0800

    ➕ADD: Example of Test Probes to simulate Actors' behavior

---
+++ src/test/scala/TestProbeSpec.scala
@@ -0,0 +1,95 @@
+import TestProbeSpec.{Master, Register, RegistrationAck, Report, SlaveWork, Work, WorkCompleted}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TestProbeSpec extends TestKit(ActorSystem("ActorSystem"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A master Actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+
+      expectMsg(RegistrationAck)
+    }
+
+    "send the work to the slave Actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka!"
+      master ! Work(workloadString)
+
+      // Testing the interaction between the master and the slave actor
+      slave.expectMsg(SlaveWork(workloadString, testActor))
+      slave.reply(WorkCompleted(3, testActor))
+
+      expectMsg(Report(3))
+    }
+
+    "aggregate data correctly" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka!"
+      master ! Work(workloadString)
+      master ! Work(workloadString)
+
+      slave.receiveWhile() {
+        case SlaveWork(`workloadString`, `testActor`) => slave.reply(WorkCompleted(3, testActor))
+      }
+
+      expectMsg(Report(3))
+      expectMsg(Report(6))
+    }
+  }
+}
+
+object TestProbeSpec {
+  //scenario
+
+  case class Register(slaveRef: ActorRef)
+
+  case class Work(text: String)
+
+  case class SlaveWork(text: String, originalRequester: ActorRef)
+
+  case class WorkCompleted(count: Int, originalRequester: ActorRef)
+
+  case class Report(totalWordCount: Int)
+
+  case object RegistrationAck
+
+  class Master extends Actor {
+    override def receive: Receive = {
+      case Register(slaveRef) =>
+        sender() ! RegistrationAck
+        context.become(online(slaveRef, 0))
+      case _ =>
+    }
+
+    def online(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequester) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequester ! Report(newTotalWordCount)
+        context.become(online(slaveRef, newTotalWordCount))
+    }
+  }
+}
