1dc43307382495434d8e671304ca65671e1534e7
kumo829 <amonmar2000@hotmail.com>
2022-08-25 21:22:15 -0800

    ➕ADD: example of maintaining state using variables with typed actors

---
+++ src/main/scala/actortyped/WalletStateApp.scala
@@ -0,0 +1,39 @@
+package actortyped
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
+
+
+object WalletState {
+  sealed trait Command
+  final case class Increase(dollars: Int) extends Command
+  final case class Decrease(dollars: Int) extends Command
+
+
+
+  def apply(total: Int, max: Int): Behavior[Command] = {
+    Behaviors.receive { (context, message) =>
+      message match {
+        case Increase(dollars) =>
+          val current = total + dollars
+          if (current <= max) {
+            context.log.info(s"Increasing to $current")
+            apply(current, max)
+          } else {
+            context.log.info(s"I'm overloaded. Counting '$current' while max is '$max'. Stopping")
+            Behaviors.stopped
+          }
+      }
+    }
+  }
+
+}
+
+object WalletStateApp extends App {
+  val guardian: ActorSystem[WalletState.Command] = ActorSystem(WalletState(0, 2), "wallet-state")
+
+  guardian ! WalletState.Increase(1)
+  guardian ! WalletState.Increase(1)
+  guardian ! WalletState.Increase(1)
+
+}
