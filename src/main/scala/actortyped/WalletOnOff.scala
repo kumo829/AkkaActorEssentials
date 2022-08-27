5097b690f4300c3bef54302bfe05f93a07bb0bc8
kumo829 <amonmar2000@hotmail.com>
2022-08-26 21:26:43 -0800

    ➕ADD: example of maintaining state using behaviors with typed actors

---
+++ src/main/scala/actortyped/WalletOnOff.scala
@@ -0,0 +1,47 @@
+package actortyped
+
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
+object WalletOnOff extends App {
+  sealed trait Command
+  final case class Increase(amount: Int) extends Command
+  final case object Deactivate extends Command
+  final case object Activate extends Command
+
+  def apply(): Behavior[Command] = activated(0)
+
+  def activated(count: Int): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case Increase(amount) =>
+          val current = count + amount
+          context.log.info(s"Increasing to $current")
+          activated(current)
+        case Deactivate => deactivated(count)
+        case Activate => Behaviors.same
+      }
+    }
+
+  def deactivated(count: Int): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case Increase(amount) =>
+          context.log.info("Wallet is deactivated. Can't connect")
+          Behaviors.same
+        case Deactivate => Behaviors.same
+        case Activate =>
+          context.log.info("activating")
+          activated(count)
+      }
+    }
+
+
+
+  val guardian: ActorSystem[Command] = ActorSystem(WalletOnOff(), "wallet-on-off")
+  guardian ! Increase(1)
+  guardian ! Deactivate
+  guardian ! Increase(1)
+  guardian ! Activate
+  guardian ! Increase(1)
+}
